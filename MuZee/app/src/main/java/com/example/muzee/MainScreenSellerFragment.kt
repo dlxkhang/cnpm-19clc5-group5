e1f91af559f1a7e1aa7bb4cfc34d5c757316123b
impac <khangduong0902@gmail.com>
2021-07-24 20:36:24 +0700

    add nav menu seller

--- MuZee/app/src/main/java/com/example/muzee/MainScreenSellerFragment.kt
+++ MuZee/app/src/main/java/com/example/muzee/MainScreenSellerFragment.kt
@@ -5,20 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.muzee.databinding.FragmentMainScreenSellerBinding
+import com.google.android.material.navigation.NavigationView
 
 
 class MainScreenSellerFragment : Fragment() {
 
+    lateinit var toggle: ActionBarDrawerToggle
+    private var binding: FragmentMainScreenSellerBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_screen_seller, container, false)
+        // setup data binding
+        val fragmentBinding = FragmentMainScreenSellerBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+
+        val drawerLayout: DrawerLayout = binding!!.drawerLayout
+        val navView: NavigationView
+        return fragmentBinding.root
+        //return inflater.inflate(R.layout.fragment_main_screen_seller, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.mainScreenSellerFragment = this // setup data binding
     }
 
 }
