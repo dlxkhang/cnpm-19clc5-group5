1ab37e7ce66a25df6749cc3a70c19f0d865034f0
impac <khangduong0902@gmail.com>
2021-07-25 16:39:48 +0700

    add menu for seller (no navigation)

--- MuZee/app/src/main/java/com/example/muzee/MainScreenSellerFragment.kt
+++ MuZee/app/src/main/java/com/example/muzee/MainScreenSellerFragment.kt
@@ -1,10 +1,10 @@
 package com.example.muzee
 
+
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.muzee.databinding.FragmentMainScreenSellerBinding
@@ -12,8 +12,9 @@ import com.example.muzee.databinding.FragmentMainScreenSellerBinding
 
 class MainScreenSellerFragment : Fragment(){
 
-    lateinit var toggle: ActionBarDrawerToggle
-    private var binding: FragmentMainScreenSellerBinding? = null
+    lateinit var drawerLayout : DrawerLayout // drawerLayout contain nav menu in xml
+    lateinit var toggle: ActionBarDrawerToggle // toggle button
+    private var binding: FragmentMainScreenSellerBinding? = null // binding fragment_main_screen_seller.xml
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,23 +24,32 @@ class MainScreenSellerFragment : Fragment(){
         val fragmentBinding = FragmentMainScreenSellerBinding.inflate(inflater, container, false)
         binding = fragmentBinding
 
-        // binding drawerLayout and navView from xml files
-        val drawerLayout: DrawerLayout = binding!!.drawerLayout
+        setHasOptionsMenu(true)
 
-        // init toggle
+        drawerLayout = binding!!.drawerLayout // binding drawerLayout from xml
+
+        // init toggle button for menu
         toggle = ActionBarDrawerToggle(activity, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        val activity = activity as AppCompatActivity? // get activity
+        activity!!.supportActionBar?.setDisplayHomeAsUpEnabled(true) // make toggle button visible
 
         return fragmentBinding.root
-        //return inflater.inflate(R.layout.fragment_main_screen_seller, container, false)
+    }
+
+    // in case toggle button is pressed
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.mainScreenSellerFragment = this // setup data binding
     }
-
 
 }
--- muzee/app/src/main/java/com/example/muzee/MainActivity.kt
+++ muzee/app/src/main/java/com/example/muzee/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.example.muzee
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.muzee.databinding.FragmentMainScreenSellerBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var navController: NavController
+    private var binding: FragmentMainScreenSellerBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
