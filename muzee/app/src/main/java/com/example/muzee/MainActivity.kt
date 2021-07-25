54bf9cb74ac5122bcd1b6a225f996354bbeb4de3
impac <khangduong0902@gmail.com>
2021-07-25 16:40:38 +0700

    Delete redundant imports

--- muzee/app/src/main/java/com/example/muzee/MainActivity.kt
+++ muzee/app/src/main/java/com/example/muzee/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.muzee
 
 import android.os.Bundle
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
