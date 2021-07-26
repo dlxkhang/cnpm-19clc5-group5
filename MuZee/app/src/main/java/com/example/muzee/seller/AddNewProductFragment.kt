04ab77a36f47a35e3baea3824c72162a0d1a4027
dlxkhang <khangduong0902@gmail.com>
2021-07-26 11:50:47 +0700

    import backup

---
+++ MuZee/app/src/main/java/com/example/muzee/seller/AddNewProductFragment.kt
@@ -0,0 +1,28 @@
+package com.example.muzee.seller
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.muzee.databinding.FragmentAddNewProductBinding
+
+
+class AddNewProductFragment : Fragment() {
+
+    private var binding: FragmentAddNewProductBinding? = null // binding fragment_add_new_product.xml
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // setup data binding
+        val fragmentBinding = FragmentAddNewProductBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+
+        val activity = activity as AppCompatActivity? // get activity
+        activity!!.supportActionBar?.setTitle("Add New Product") // set title text for seller product screen
+
+        return fragmentBinding.root
+    }
+}
