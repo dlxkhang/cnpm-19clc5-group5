2099893c6c206ca5ea5bae2a2394490b53704b60
ThoChung <chungthetho@gmail.com>
2021-07-27 20:59:04 +0700

    Update product detail

--- MuZee/app/src/main/java/com/example/muzee/data/Datasource.kt
+++ MuZee/app/src/main/java/com/example/muzee/data/Datasource.kt
@@ -4,13 +4,13 @@ class Datasource {
 
     fun loadProduct(): List<Product> {
         return listOf<Product>(
-            Product("Nord Piano 4 88-Key Digital Piano", 2.999),
+            Product("Nord Piano 4 88-Key Digital Piano", 2999.0),
             Product("Guitar Bass Yamaha TRBX174", 200.0),
             Product("Yamaha Revstar RS620 Electronic Guitar", 714.0),
-            Product("Nord Piano 4 88-Key Digital Piano", 2.999),
+            Product("Nord Piano 4 88-Key Digital Piano", 2999.0),
             Product("Guitar Bass Yamaha TRBX174", 200.0),
             Product("Yamaha Revstar RS620 Electronic Guitar", 714.0),
-            Product("Nord Piano 4 88-Key Digital Piano", 2.999),
+            Product("Nord Piano 4 88-Key Digital Piano", 2999.0),
             Product("Guitar Bass Yamaha TRBX174", 200.0),
             Product("Yamaha Revstar RS620 Electronic Guitar", 714.0)
         )
