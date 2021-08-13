132223d0cfc3fce056b5848a3c640cf2312fdf1d
dlxkhang <khangduong0902@gmail.com>
2021-08-13 12:21:01 +0700

    add/edit/delete new product

---
+++ Back_end/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const router = require('./router/server_router');
+
+// express declarations
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing
+
+
+var port = 3000;
+
+
+app.use('/', router);
+
+
+app.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
